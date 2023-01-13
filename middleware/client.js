44eec06c3ccf3293978d2e8ba1396d9038fca3e9
Tsihoarana <raoseta@gmail.com>
2023-01-13 17:56:45 +0300

    fix typo

--- middleware/client.js
+++ middleware/client.js
@@ -2,8 +2,8 @@ module.exports = function(req, res, next) {
   // 401 Unauthorized
   // 403 Forbidden
   // if (!config.get("requiresAuth")) return next();
-  console.log("ito" + req.user.code_type);
-  if (req.user.codeType > 0) return res.status(403).send("Access denied.");
+
+  if (req.user.code_type !== 0) return res.status(403).send("Access denied.");
 
   next();
 };
